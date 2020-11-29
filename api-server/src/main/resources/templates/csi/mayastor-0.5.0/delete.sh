b940e221b03c286991ed3d9315cfcc1df67baf2e
Kidong Lee <mykidong@gmail.com>
2020-11-29 15:36:51 +0900

    jinja raw added...

--- api-server/src/main/resources/templates/csi/mayastor-0.5.0/delete.sh
+++ api-server/src/main/resources/templates/csi/mayastor-0.5.0/delete.sh
@@ -16,6 +16,7 @@ IFS=' ';
 #Read the split words into an array based on space delimiter
 read -a MSP_ARRAY <<< "${MSP}";
 
+{% raw %}
 #Count the total words
 echo "MSP_ARRAY count: ${#MSP_ARRAY[*]}";
 
@@ -24,3 +25,4 @@ for ((i = 0; i < ${#MSP_ARRAY[@]}; ++i)); do
     printf "MSP_NAME: %s\n" "${MSP_NAME}";
     kubectl -n mayastor get msp ${MSP_NAME} -o yaml > msp.yaml &&  sed 's/- finalizer.mayastor.openebs.io//' msp.yaml | kubectl replace -f -
 done
+{% endraw %}
