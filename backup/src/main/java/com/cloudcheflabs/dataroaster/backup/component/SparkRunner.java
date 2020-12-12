b28b3cbfb25de49e5e226afdd41b9d3833e7786a
Kidong Lee <mykidong@gmail.com>
2020-12-12 18:09:20 +0900

    backup test case added...

---
+++ backup/src/main/java/com/cloudcheflabs/dataroaster/backup/component/SparkRunner.java
@@ -0,0 +1,36 @@
+package com.cloudcheflabs.dataroaster.backup.component;
+
+/**
+ * Created by mykidong on 2017-11-27.
+ */
+public interface SparkRunner {
+
+    public static enum CriterionDate {
+        NOW, NULL;
+    }
+
+    public static enum DateType {
+        DAY, HOUR
+    }
+
+    public static enum DateDirPath
+    {
+        App("app="), Year("year="), Month("month="), Day("day="), Hour("hour=");
+
+        private String dirPath;
+
+        private DateDirPath(String dirPath)
+        {
+            this.dirPath = dirPath;
+        }
+
+        public String getDirPath()
+        {
+            return this.dirPath;
+        }
+    }
+
+
+    int run(String[] args) throws Exception;
+
+}
---
+++ backup/src/test/java/com/cloudcheflabs/dataroaster/backup/component/S3ToAWSS3BackupTestRunner.java
@@ -0,0 +1,37 @@
+package com.cloudcheflabs.dataroaster.backup.component;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Test;
+
+public class S3ToAWSS3BackupTestRunner {
+
+    @Test
+    public void s3ToAwsS3Backup() throws Exception {
+
+        String master = "local[2]";
+
+        SparkConf sparkConf = new SparkConf().setAppName(S3ToAWSS3BackupTestRunner.class.getName());
+        sparkConf.setMaster(master);
+
+        SparkSession spark = SparkSession
+                .builder()
+                .config(sparkConf)
+                .getOrCreate();
+
+        Configuration hadoopConfiguration = spark.sparkContext().hadoopConfiguration();
+        hadoopConfiguration.set("fs.defaultFS", "s3a://mykidong");
+        hadoopConfiguration.set("fs.s3a.endpoint", "https://smartlife-tenant.minio.cloudchef-labs.com");
+        hadoopConfiguration.set("fs.s3a.access.key", "bWluaW8=");
+        hadoopConfiguration.set("fs.s3a.secret.key", "bWluaW8xMjM=");
+        hadoopConfiguration.set("fs.s3a.path.style.access", "true");
+        hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
+
+        Dataset<Row> df = spark.read().format("parquet").load("/test-parquet");
+
+        df.show(10);
+    }
+}
