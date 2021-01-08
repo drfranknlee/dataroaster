aea5abc9913b1f8a2ce117cc59289d3b48e58726
Kidong Lee <mykidong@gmail.com>
2021-01-08 14:19:30 +0900

    timeout added to executor to prevent task from waiting for finishing the job running for a long time...

--- api-server/src/main/java/com/cloudcheflabs/dataroaster/apiserver/kubernetes/ExecutorUtils.java
+++ api-server/src/main/java/com/cloudcheflabs/dataroaster/apiserver/kubernetes/ExecutorUtils.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
@@ -14,19 +15,18 @@ public class ExecutorUtils {
 
     public static <T> void runTask(Callable<T> task) {
         ExecutorService executor = Executors.newFixedThreadPool(1);
-        List<Future<T>> futureList = new ArrayList<Future<T>>();
-        for(int i = 0; i < 1; i++) {
-            Future<T> future = executor.submit(task);
-            futureList.add(future);
+        try {
+            // execute task with timeout.
+            List<Future<T>> futureList =
+                    executor.invokeAll(Arrays.asList(task), 15, TimeUnit.MINUTES);
+
+            for (Future<T> fut : futureList) {
+                LOG.info(new Date() + "::" + fut.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            LOG.error(e.getMessage());
         }
 
-        for (Future<T> fut : futureList) {
-            try {
-                LOG.info(new Date() + "::" + fut.get());
-            } catch (InterruptedException | ExecutionException e) {
-                LOG.error(e.getMessage());
-            }
-        }
         executor.shutdown();
     }
 }
