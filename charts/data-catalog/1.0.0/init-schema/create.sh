ae2b6bb3a8d00ab21f9ea25cb4a151965b556b52
Kidong Lee <mykidong@gmail.com>
2021-08-24 15:43:37 +0900

    kubectl wait added for init schema job...

--- charts/data-catalog/1.0.0/init-schema/create.sh
+++ charts/data-catalog/1.0.0/init-schema/create.sh
@@ -15,3 +15,9 @@ helm install \
 ${APP_NAME} \
 --values dataroaster-values.yaml \
 ./;
+
+
+# wait.
+kubectl wait --namespace ${NAMESPACE} \
+  --for=condition=complete job/hive-initschema \
+  --timeout=120s
