eadea05075327ee43f9278dccba684480a7e3f5a
Kidong Lee <mykidong@gmail.com>
2021-09-06 03:10:53 +0900

    dataroaster db restricted to be accessed just from localhost

--- authorizer/sql/create-db-user.sql
+++ authorizer/sql/create-db-user.sql
@@ -1,14 +1,6 @@
 # create db and user.
 CREATE DATABASE dataroaster;
  
-CREATE USER 'dataroaster'@'%' IDENTIFIED BY 'icarus0337';
-GRANT ALL PRIVILEGES ON *.* TO 'dataroaster'@'%' WITH GRANT OPTION;
-flush privileges;
- 
 CREATE USER 'dataroaster'@'localhost' IDENTIFIED BY 'icarus0337';
 GRANT ALL PRIVILEGES ON *.* TO 'dataroaster'@'localhost' WITH GRANT OPTION;
-flush privileges;
-  
-CREATE USER 'dataroaster'@'dataroaster' IDENTIFIED BY 'icarus0337';
-GRANT ALL PRIVILEGES ON *.* TO 'dataroaster'@'dataroaster' WITH GRANT OPTION;
 flush privileges;
